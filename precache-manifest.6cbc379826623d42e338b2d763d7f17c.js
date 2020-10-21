7159513194aaff71de3da8323174f905453b8cd8
jeremyCharlesDev <jrmprod@gmail.com>
2020-10-21 16:48:22 +0200

    Updates

---
+++ precache-manifest.6cbc379826623d42e338b2d763d7f17c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f562aa0faccbe60eeb5da753e7cd9ab",
+    "url": "/twitchclonereact/index.html"
+  },
+  {
+    "revision": "abdf35659f0679ec24b3",
+    "url": "/twitchclonereact/static/css/main.2df7da0d.chunk.css"
+  },
+  {
+    "revision": "2e5dff4418fa00fe4c62",
+    "url": "/twitchclonereact/static/js/2.7c81cb43.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/twitchclonereact/static/js/2.7c81cb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abdf35659f0679ec24b3",
+    "url": "/twitchclonereact/static/js/main.4f0257ab.chunk.js"
+  },
+  {
+    "revision": "9fcb870adf1a93bb21da",
+    "url": "/twitchclonereact/static/js/runtime-main.539e4466.js"
+  },
+  {
+    "revision": "a55a5aa33744d9d0c759d774f4fea92a",
+    "url": "/twitchclonereact/static/media/Croix.a55a5aa3.svg"
+  },
+  {
+    "revision": "59ad91f94431ae27a96566bfa6b72005",
+    "url": "/twitchclonereact/static/media/IconeTwitch.59ad91f9.svg"
+  },
+  {
+    "revision": "970dcdb3e9a34218802e3149fdde5fef",
+    "url": "/twitchclonereact/static/media/MenuIco.970dcdb3.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/twitchclonereact/static/media/Search.c6a0effb.svg"
+  }
+]);
